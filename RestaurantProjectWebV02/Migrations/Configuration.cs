6497cd25ba1f5bda7d151f89af3f81600754c4a3
ingTorok <toroklehel89@gmail.com>
2019-03-17 09:34:54 +0100

    PM enable-migration PM add-migration "Identity datamodel TL'

---
+++ RestaurantProjectWebV02/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace RestaurantProjectWebV02.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<RestaurantProjectWebV02.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(RestaurantProjectWebV02.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
