c02938e77212d4eef7f6c427fa2d65416cdbb58d
ingTorok <toroklehel89@gmail.com>
2019-03-17 10:40:52 +0100

    PM Set MenuItems.Name maxlength to 200

--- RestaurantProjectWebV02/Models/MenuItem.cs
+++ RestaurantProjectWebV02/Models/MenuItem.cs
@@ -17,6 +17,7 @@ namespace RestaurantProjectWebV02.Models
         public int Id { get; set; }
 
         [Required]
+        [StringLength(200)]
         public string Name { get; set; }
 
         [Required]
