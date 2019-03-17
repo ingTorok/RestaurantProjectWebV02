4fe02fa7bea768d36b2bf77e7fc01219fce87aab
ingTorok <toroklehel89@gmail.com>
2019-03-17 10:19:15 +0100

    Created Class MenuItem

---
+++ RestaurantProjectWebV02/Models/MenuItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestaurantProjectWebV02.Models
+{
+    /// <summary>
+    /// One item on Menu
+    /// </summary>
+    public class MenuItem
+    {
+        /// <summary>
+        /// Primary Key PK
+        /// </summary>
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+    }
+}
