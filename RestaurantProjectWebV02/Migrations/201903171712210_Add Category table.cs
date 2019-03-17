9f9531feb0cc9f6cfb98f86365ac736595972da5
ingTorok <toroklehel89@gmail.com>
2019-03-17 18:13:37 +0100

    PM Add Categries table

---
+++ RestaurantProjectWebV02/Migrations/201903171712210_Add Category table.cs
@@ -0,0 +1,32 @@
+namespace RestaurantProjectWebV02.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddCategorytable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+            AddColumn("dbo.MenuItems", "Category_Id", c => c.Int(nullable: false));
+            CreateIndex("dbo.MenuItems", "Category_Id");
+            AddForeignKey("dbo.MenuItems", "Category_Id", "dbo.Categories", "Id", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.MenuItems", "Category_Id", "dbo.Categories");
+            DropIndex("dbo.MenuItems", new[] { "Category_Id" });
+            DropColumn("dbo.MenuItems", "Category_Id");
+            DropTable("dbo.Categories");
+        }
+    }
+}
--- RestaurantProjectWebV02/Models/MenuItem.cs
+++ RestaurantProjectWebV02/Models/MenuItem.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace RestaurantProjectWebV02.Models
 {
@@ -25,5 +27,16 @@ namespace RestaurantProjectWebV02.Models
 
         [Required]
         public double Price { get; set; }
+
+        [Required]
+        public Category Category { get; set; }
+
+        #region Only for the View
+        [NotMapped] //we dont need for the database
+        public List<SelectListItem> AssignableCategories { get; set; }
+
+        [NotMapped] //we dont need for the database
+        public int CategoryId { get; set; }
+        #endregion Only for the View    
     }
 }
